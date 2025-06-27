5f6e127ba8e2216c82dfca84642556a31328be4e
Lai lipeng <100109981+Lipeng-Lai@users.noreply.github.com>
2025-06-27 23:38:04 +0800

    Create config.py

---
+++ configs/config.py
@@ -0,0 +1,20 @@
+import os
+import torch
+from omegaconf import OmegaConf
+
+def get_config(path):
+    config = OmegaConf.load(path)
+    config.gpu_num = torch.cuda.device_count()
+    overrides = OmegaConf.from_cli()
+    config = OmegaConf.merge(config, overrides)
+    output_dir = os.path.join(config.train.output, config.model.name)
+    os.makedirs(output_dir, exist_ok=True)
+    path = os.path.join(output_dir, "config.yaml")
+    with open(path, 'w') as f:
+        OmegaConf.save(config, f)
+    return config
+
+if __name__ == '__main__':
+    config_path = 'config.yaml'
+    config = get_config(config_path)
+    
